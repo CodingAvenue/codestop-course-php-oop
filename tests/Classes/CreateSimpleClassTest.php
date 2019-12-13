455eb236bd11b79f78f4dd6702f2d1e63da6d365
Cerila Canda <cerila.canda@codingavenue.com>
2019-12-13 16:40:59 +0800

    CC-611 Edited test files; adjusted spacing and alignment

--- tests/Classes/CreateSimpleClassTest.php
+++ tests/Classes/CreateSimpleClassTest.php
@@ -6,70 +6,72 @@ class CreateSimpleClassTest extends Proof
 	public function testPhpStartTag()
 	{
 		$checkStart = self::$code->codeStartCheck();
+
 		$this->assertEquals(true, $checkStart, "Expecting the `<?php` tag on the first line.");
-    }
-    
+	}
+
 	public function testActualCode()
 	{
 		$evaluator = self::$code->evaluator();
 		$evaled    = $evaluator->evaluate();
 		$expected  = "This is an eat method.";
+
 		$this->assertEquals($expected, $evaled['output'], "Expected output is \"$expected\".");
-    }
-    
-    public function testEcho()
+	}
+
+	public function testEcho()
 	{
-		$nodes=self::$code->find('construct[name="echo"]');
-		
+		$nodes = self::$code->find('construct[name="echo"]');
+
 		$this->assertEquals(1, $nodes->count(), "Expecting a single echo statement.");
 	}
 
 	public function testAssignment()
-    {
-        $nodes = self::$code->find('operator[name="assignment"]');
-	
-        $this->assertEquals(1, $nodes->count(), "Expecting an assignment statement that assigns a value to the variable 'pObject'.");
+	{
+		$nodes = self::$code->find('operator[name="assignment"]');
+
+		$this->assertEquals(1, $nodes->count(), "Expecting an assignment statement that assigns a value to the variable 'pObject'.");
 	}
-	
+
 	public function testpObjectVariable()
-    {
-        $pObject = self::$code->find('variable[name="pObject"]');
-        
-        $this->assertEquals(2, $pObject->count(), "Expecting two occurrences of the variable named 'pObject'.");
+	{
+		$pObject = self::$code->find('variable[name="pObject"]');
+
+		$this->assertEquals(2, $pObject->count(), "Expecting two occurrences of the variable named 'pObject'.");
 	}
 
 	public function testInstantiation()
-    {
-        $nodes=self::$code->find('instantiate[class="Person"]');
-		
-        $this->assertEquals(1, $nodes->count(), "Expecting an instantiation statement of the 'Person' class.");
+	{
+		$nodes = self::$code->find('instantiate[class="Person"]');
+
+		$this->assertEquals(1, $nodes->count(), "Expecting an instantiation statement of the 'Person' class.");
 	} 
 
 	public function testEat()
-    {
-        $eat=self::$code->find('method[name="eat"]');
-        
-        $this->assertEquals(1, $eat->count(), "Expecting an 'eat()' method.");
+	{
+		$eat = self::$code->find('method[name="eat"]');
+
+		$this->assertEquals(1, $eat->count(), "Expecting an 'eat()' method.");
 	}
-	
+
 	public function testNameProperty()
-    {
-        $name = self::$code->find('property[name="name", type="public"]');
-        
-        $this->assertEquals(1, $name->count(), "Expecting a public class property named 'name'.");
+	{
+		$name = self::$code->find('property[name="name", type="public"]');
+
+		$this->assertEquals(1, $name->count(), "Expecting a public class property named 'name'.");
 	}
 
 	public function testClass()
-    {
-        $nodes=self::$code->find('class[name="Person"]');
-		
-        $this->assertEquals(1, $nodes->count(), "Expecting a class declaration of the `Person` class.");
+	{
+		$nodes = self::$code->find('class[name="Person"]');
+
+		$this->assertEquals(1, $nodes->count(), "Expecting a class declaration of the `Person` class.");
 	}  
-	
+
 	public function testEatCall()
-    {
-        $eat = self::$code->find('method-call[name="eat", variable="pObject"]');
-        
-        $this->assertEquals(1, $eat->count(), "Expecting an 'eat()' method call of 'pObject'.");
+	{
+		$eat = self::$code->find('method-call[name="eat", variable="pObject"]');
+
+		$this->assertEquals(1, $eat->count(), "Expecting an 'eat()' method call of 'pObject'.");
 	}
 }
--- tests/Classes/MissingFunctionKeywordTest.php
+++ tests/Classes/MissingFunctionKeywordTest.php
@@ -6,70 +6,72 @@ class MissingFunctionKeywordTest extends Proof
 	public function testPhpStartTag()
 	{
 		$checkStart = self::$code->codeStartCheck();
+
 		$this->assertEquals(true, $checkStart, "Expecting the `<?php` tag on the first line.");
-    }
-    
+	}
+
 	public function testActualCode()
 	{
 		$evaluator = self::$code->evaluator();
 		$evaled    = $evaluator->evaluate();
 		$expected  = "This is a class method.";
+
 		$this->assertEquals($expected, $evaled['output'], "Expected output is \"$expected\".");
-    }
-    
-    public function testEcho()
+	}
+
+	public function testEcho()
 	{
-		$nodes=self::$code->find('construct[name="echo"]');
-		
+		$nodes = self::$code->find('construct[name="echo"]');
+
 		$this->assertEquals(1, $nodes->count(), "Expecting a single echo statement.");
 	}
 
 	public function testAssignment()
-    {
-        $nodes = self::$code->find('operator[name="assignment"]');
-	
-        $this->assertEquals(1, $nodes->count(), "Expecting an assignment statement that assigns a value to the variable 'myObject'.");
+	{
+		$nodes = self::$code->find('operator[name="assignment"]');
+
+		$this->assertEquals(1, $nodes->count(), "Expecting an assignment statement that assigns a value to the variable 'myObject'.");
 	}
-	
+
 	public function testMyObjectVariable()
-    {
-        $myObject = self::$code->find('variable[name="myObject"]');
-        
-        $this->assertEquals(2, $myObject->count(), "Expecting two occurrences of the variable named 'myObject'.");
+	{
+		$myObject = self::$code->find('variable[name="myObject"]');
+
+		$this->assertEquals(2, $myObject->count(), "Expecting two occurrences of the variable named 'myObject'.");
 	}
 
 	public function testInstantiation()
-    {
-        $nodes=self::$code->find('instantiate[class="MyClass"]');
-		
-        $this->assertEquals(1, $nodes->count(), "Expecting an instantiation statement of the 'MyClass' class.");
+	{
+		$nodes = self::$code->find('instantiate[class="MyClass"]');
+
+		$this->assertEquals(1, $nodes->count(), "Expecting an instantiation statement of the 'MyClass' class.");
 	} 
 
 	public function testMyMethod()
-    {
-        $myMethod=self::$code->find('method[name="myMethod"]');
-        
-        $this->assertEquals(1, $myMethod->count(), "Expecting a myMethod() method.");
+	{
+		$myMethod = self::$code->find('method[name="myMethod"]');
+
+		$this->assertEquals(1, $myMethod->count(), "Expecting a myMethod() method.");
 	}
-	
+
 	public function testMyPropVariable()
-    {
-        $myProp = self::$code->find('property[name="myProp", type="public"]');
-        
-        $this->assertEquals(1, $myProp->count(), "Expecting a public class property named 'myProp'.");
+	{
+		$myProp = self::$code->find('property[name="myProp", type="public"]');
+
+		$this->assertEquals(1, $myProp->count(), "Expecting a public class property named 'myProp'.");
 	}
 
 	public function testClass()
-    {
-        $nodes=self::$code->find('class[name="MyClass"]');
-		
-        $this->assertEquals(1, $nodes->count(), "Expecting a class declaration of the `MyClass` class.");
+	{
+		$nodes = self::$code->find('class[name="MyClass"]');
+
+		$this->assertEquals(1, $nodes->count(), "Expecting a class declaration of the `MyClass` class.");
 	}  
-	
+
 	public function testMyMethodCall()
-    {
-        $myProp = self::$code->find('method-call[name="myMethod", variable="myObject"]');
-        
-        $this->assertEquals(1, $myProp->count(), "Expecting a 'myMethod()' method call of 'myObject'.");
+	{
+		$myProp = self::$code->find('method-call[name="myMethod", variable="myObject"]');
+
+		$this->assertEquals(1, $myProp->count(), "Expecting a 'myMethod()' method call of 'myObject'.");
 	}
 }
