adaddf53f76535382ffebe134e6b140c70e45d91
Cerila Canda <cerila.canda@codingavenue.com>
2019-12-13 15:37:24 +0800

    CC-613 Edited test files; adjusted spacing and alignment

--- tests/Properties/ManipulateObjectPropertiesTest.php
+++ tests/Properties/ManipulateObjectPropertiesTest.php
@@ -3,81 +3,83 @@ use CodingAvenue\Proof\Code;
 use Proofs\Proof;
 class ManipulateObjectPropertiesTest extends Proof
 {
-    
+
     public function testPhpStartTag()
     {
         $checkStart = self::$code->codeStartCheck();
+
         $this->assertEquals(true, $checkStart, "Expecting the `<?php` tag on the first line.");
     }
-    
+
     public function testActualCode()
     {
         $evaluator = self::$code->evaluator();
         $evaled    = $evaluator->evaluate();
         $expected  = "Chihuahua";
+
         $this->assertEquals($expected, $evaled['output'], "Expected output is \"$expected\".");
     }
-    
+
     public function testEcho()
     {
-        $nodes=self::$code->find('construct[name="echo"]');
-		
+        $nodes = self::$code->find('construct[name="echo"]');
+
         $this->assertEquals(2, $nodes->count(), "Expecting two echo statements.");
     }
-    
-	public function testAssignment()
+
+    public function testAssignment()
     {
         $nodes = self::$code->find('operator[name="assignment"]');
-	
+
         $this->assertEquals(2, $nodes->count(), "Expecting two assignment statements.");
-	}
-	
-	public function testPetVariable()
+    }
+
+    public function testPetVariable()
     {
         $pet = self::$code->find('variable[name="pet"]');
-        
+
         $this->assertEquals(3, $pet->count(), "Expecting three occurrences of the variable named 'pet'.");
     }
-    
-	public function testInstantiation()
+
+    public function testInstantiation()
     {
-        $nodes=self::$code->find('instantiate[class="Animal"]');
-		
+        $nodes = self::$code->find('instantiate[class="Animal"]');
+
         $this->assertEquals(1, $nodes->count(), "Expecting an instantiation statement of the 'Animal' class.");
     } 
-    
-	public function testMove()
+
+    public function testMove()
     {
-        $move=self::$code->find('method[name="move"]');
-        
+        $move = self::$code->find('method[name="move"]');
+
         $this->assertEquals(1, $move->count(), "Expecting a move() method.");
-	}
-	
-	public function testTypeProperty()
+    }
+
+    public function testTypeProperty()
     {
         $type = self::$code->find('property[name="type", type="public"]');
-        
+
         $this->assertEquals(1, $type->count(), "Expecting a public class property named 'type'.");
     }
-	
-	public function testBreedProperty()
+
+    public function testBreedProperty()
     {
         $breed = self::$code->find('property[name="breed", type="public"]');
-        
+
         $this->assertEquals(1, $breed->count(), "Expecting a public class property named 'breed'.");
     }    
 
-	public function testClass()
+    public function testClass()
     {
-        $nodes=self::$code->find('class[name="Animal"]');
-		
+        $nodes = self::$code->find('class[name="Animal"]');
+
         $this->assertEquals(1, $nodes->count(), "Expecting a class declaration of the `Animal` class.");
-	}  
-	
-	public function testBreedCall()
+    }  
+
+    public function testBreedCall()
     {
         $breed = self::$code->find('property-call[name="breed", variable="pet"]');
-        
+
         $this->assertEquals(2, $breed->count(), "Expecting two 'breed' property calls of 'pet'.");
-	}
+    }
 }  
--- tests/Properties/MissingDollarSignOnObjectTest.php
+++ tests/Properties/MissingDollarSignOnObjectTest.php
@@ -3,81 +3,83 @@ use CodingAvenue\Proof\Code;
 use Proofs\Proof;
 class MissingDollarSignOnObjectTest extends Proof
 {
-    
+
     public function testPhpStartTag()
     {
         $checkStart = self::$code->codeStartCheck();
+
         $this->assertEquals(true, $checkStart, "Expecting the `<?php` tag on the first line.");
     }
-    
+
     public function testActualCode()
     {
         $evaluator = self::$code->evaluator();
         $evaled    = $evaluator->evaluate();
         $expected  = "Canada";
+
         $this->assertEquals($expected, $evaled['output'], "Expected output is \"$expected\".");
     }
-    
+
     public function testEcho()
     {
-        $nodes=self::$code->find('construct[name="echo"]');
-		
+        $nodes = self::$code->find('construct[name="echo"]');
+
         $this->assertEquals(2, $nodes->count(), "Expecting two echo statements.");
     }
-    
-	public function testAssignment()
+
+    public function testAssignment()
     {
         $nodes = self::$code->find('operator[name="assignment"]');
-	
+
         $this->assertEquals(2, $nodes->count(), "Expecting two assignment statements.");
-	}
-	
-	public function testPersonObjectVariable()
+    }
+
+    public function testPersonObjectVariable()
     {
         $personObject = self::$code->find('variable[name="personObject"]');
-        
+
         $this->assertEquals(3, $personObject->count(), "Expecting three occurrences of the variable named 'personObject'.");
     }
-    
-	public function testInstantiation()
+
+    public function testInstantiation()
     {
-        $nodes=self::$code->find('instantiate[class="Person"]');
-		
+        $nodes = self::$code->find('instantiate[class="Person"]');
+
         $this->assertEquals(1, $nodes->count(), "Expecting an instantiation statement of the 'Person' class.");
     } 
-    
-	public function testEat()
+
+    public function testEat()
     {
-        $eat=self::$code->find('method[name="eat"]');
-        
+        $eat = self::$code->find('method[name="eat"]');
+
         $this->assertEquals(1, $eat->count(), "Expecting an eat() method.");
-	}
-	
-	public function testNameProperty()
+    }
+
+    public function testNameProperty()
     {
         $name = self::$code->find('property[name="name", type="public"]');
-        
+
         $this->assertEquals(1, $name->count(), "Expecting a public class property named 'name'.");
     }
-	
-	public function testAddressProperty()
+
+    public function testAddressProperty()
     {
         $address = self::$code->find('property[name="address", type="public"]');
-        
+
         $this->assertEquals(1, $address->count(), "Expecting a public class property named 'address'.");
     }    
 
-	public function testClass()
+    public function testClass()
     {
-        $nodes=self::$code->find('class[name="Person"]');
-		
+        $nodes = self::$code->find('class[name="Person"]');
+
         $this->assertEquals(1, $nodes->count(), "Expecting a class declaration of the `Person` class.");
-	}  
-	
-	public function testAddressCall()
+    }  
+
+    public function testAddressCall()
     {
         $address = self::$code->find('property-call[name="address", variable="personObject"]');
-        
+
         $this->assertEquals(2, $address->count(), "Expecting two 'address' property calls of 'personObject'.");
-	}
+    }
 }  
